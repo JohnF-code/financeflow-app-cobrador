d3367d7046017c36ceb494a725e2acd158aa85cd
JohnF-code <johnfbohorquez18@gmail.com>
2025-10-10 13:14:38 -0500

    v6.0: Fix cobrador_id filter in prestamos

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker para App Cobrador - Soporte offline completo
-const CACHE_NAME = 'cobrador-cache-v5.9';
+const CACHE_NAME = 'cobrador-cache-v6.0';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
